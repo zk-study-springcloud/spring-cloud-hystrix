1eab1e9c64c633cacd611614184299811654742c
zhaokai <506683059@qq.com>
2019-06-22 23:05:56 +0800

    feign增加Hystrix Dashboard

--- spring-cloud-feign/src/main/java/com/github/zk/springcloudfeign/SpringCloudFeignApplication.java
+++ spring-cloud-feign/src/main/java/com/github/zk/springcloudfeign/SpringCloudFeignApplication.java
@@ -3,11 +3,13 @@ package com.github.zk.springcloudfeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.github.zk.springcloudfeign.remote")
+@EnableHystrixDashboard
 public class SpringCloudFeignApplication {
 
     public static void main(String[] args) {
